d016039468bc346cc7bdf11bbca7fb0202c965e7
Zach Rankin <zrankin@nmu.edu>
2025-03-01 15:53:38 -0500

    Made changes

---
+++ Java_code/Examples/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        for ( int i = 1; i <= 10; ++i ){
+            System.out.print(i + " ");
+        }
+
+
+
+        int count = 1;
+        while( count < 11){
+            System.out.println(1 + " ");
+            count += 1;
+        }
+
+
+    }
+}
