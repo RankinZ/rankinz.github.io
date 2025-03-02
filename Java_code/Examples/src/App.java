ff613f7e074c8b31e046a526cbaded02ebbe8a76
Zach Rankin <zrankin@nmu.edu>
2025-03-02 15:56:20 -0500

    java examples

--- Java_code/Examples/src/App.java
+++ Java_code/Examples/src/App.java
@@ -2,17 +2,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         
 
-        for ( int i = 1; i <= 10; ++i ){
-            System.out.print(i + " ");
+        for ( int i = 1; i <= 20; ++i ){
+            System.out.print("Hello, World!");
         }
 
 
 
-        int count = 1;
-        while( count < 11){
-            System.out.println(1 + " ");
-            count += 1;
-        }
+int count = 1;
+while( count <= 20){
+    System.out.println("Hello, World!");
+    count += 1;
+}
 
 
     }
